88e5b3622215ac4c3f5a34833d9fbcdce98bc7ec
Sethu-madhav <sethu4784@gmail.com>
2022-09-01 06:06:41 +0530

    Added header and navigation menu
    Added header and navigation menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,42 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
+/*===============   MENU SHOW =============== */
+// Validate if constant exists
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 
-/*=============== REMOVE MENU MOBILE ===============*/
+/*===============   MENU HIDE =============== */
+// Validate if constant exists
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
+/*=============== REMOVE MENU MOBILE =============== */
+const navLink = document.querySelectorAll(".nav__link");
 
-/*=============== CHANGE BACKGROUND HEADER ===============*/
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
+/*=============== CHANGE BACKGROUND HEADER =============== */
 
-/*=============== QUESTIONS ACCORDION ===============*/
+/*=============== QUESTIONS ACCORDION =============== */
 
+/*=============== SCROLL SECTIONS ACTIVE LINK =============== */
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+/*=============== SHOW SCROLL UP =============== */
 
+/*=============== DARK LIGHT THEME =============== */
 
-/*=============== SHOW SCROLL UP ===============*/ 
-
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
-
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+/*=============== SCROLL REVEAL ANIMATION =============== */
